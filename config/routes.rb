7b9c863ebd4a573af4f0c36f19c7ab1c39a7a40c
Akito Mizuno <mznakt@gmail.com>
2018-03-11 19:18:39 +0900

    add post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'presents#index'
+  resources :presents, only: %i(index) do
+    post 'send_code', on: :collection
+  end
 end
